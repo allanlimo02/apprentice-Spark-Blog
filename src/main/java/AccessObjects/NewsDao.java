0e473f463da2bf1f807882a1fb7cfbfa260cc05b
allanlimo02 <allanlimo02@gmail.com>
2022-01-25 09:11:58 +0300

    Created News  and User POJO classes and their access methods

---
+++ src/main/java/AccessObjects/NewsDao.java
@@ -0,0 +1,53 @@
+package AccessObjects;
+
+import AccessInterface.ConnnectionNewsInterface;
+import Models.News;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class NewsDao implements ConnnectionNewsInterface {
+
+
+    Sql2o sql2o;
+
+    public NewsDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public static void add(News news) {
+        String sql ="INSERT INTO news (description) VALUES (:descriptiom)";
+        try (Connection conn= sql2o.open()){
+                int id=(int) conn.createQuery(sql,true)
+                        .bind(news)
+                        .executeUpdate()
+                        .getKey();
+                news.setSerialNo(id);
+        }catch (Sql2oException sql2oException){
+            sql2oException.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public List<News> getAll() {
+        String sql="SELECT * FROM news";
+        try (Connection connection=sql2o.open()){
+            return connection.createQuery(sql)
+                    .executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public News findById(int serialNo) {
+        String sql="SELECT * FROM news WHERE serialNo = :serialNo";
+        try (Connection conn= sql2o.open()){
+            return conn.createQuery(sql)
+                    .addParameter("serialNo",serialNo)
+                    .executeAndFetchFirst(News.class);
+        }
+    }
+}
