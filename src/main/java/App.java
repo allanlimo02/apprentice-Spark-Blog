db146d9f00ac6ecbfc6cabe4cdb8a7c716a5840e
allanlimo02 <allanlimo02@gmail.com>
2022-01-25 13:52:44 +0300

    Created Data access objects

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,8 @@ import static spark.Spark.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import AccessObjects.UsersDao;
+import Models.Users;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -9,6 +11,8 @@ public class App {
 
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+
 
         get("/", (request, response) -> {
             return new ModelAndView(new HashMap(), "homepage.hbs");
@@ -33,6 +37,17 @@ public class App {
             model.put("username",username);
             model.put("password",passwordInput);
             System.out.println("username ="+username+" password"+passwordInput);
+            return new ModelAndView(model, "landingpage.hbs");
+        }, new HandlebarsTemplateEngine());
+        post("/newuser", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String username=request.queryParams("username");
+            String email=request.queryParams("email");
+            String password=request.queryParams("password");
+            Users user=new Users(username,email,password);
+            UsersDao usersDao=new UsersDao();
+            usersDao.add(user);
+            System.out.println("username ="+username+" password"+password);
             return new ModelAndView(model, "landingpage.hbs");
         }, new HandlebarsTemplateEngine());
     }
